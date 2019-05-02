ab3b9a9a4f8f3ca5f24bb98d5f9bcdb66bcc5ef7
mzaini30 <muhzaini30@gmail.com>
2019-05-02 11:03:58 +0800

    oke

--- merancang/app/app.js
+++ merancang/app/app.js
@@ -8,6 +8,18 @@ $(".navbar-nav a").click(function(){
 	$('.navbar-toggle').click()
 })
 
-$('.table tr td:nth-child(1)').css({
-	'background': 'green'
+// $('.table tr td:nth-child(1)').css({
+// 	'background': 'green'
+// })
+
+$('input').on('keyup', function(){
+	$('.table tr').each(function(){
+		satu = $(this).find('td:nth-child(1) .form-control').val()
+		dua = $(this).find('td:nth-child(2) .form-control').val()
+		tiga = parseInt(satu) * parseInt(dua)
+		$(this).find('td:nth-child(3) .form-control').val(tiga)
+		// $(this).find('td:nth-child(1)').css({
+		// 	'background': 'green'
+		// })
+	})
 })
