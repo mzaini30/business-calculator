807106eff95c392c6d3d4e221ab95c4878a7e96b
mzaini30 <muhzaini30@gmail.com>
2019-05-02 14:18:40 +0800

    oke

--- merancang/app/app.js
+++ merancang/app/app.js
@@ -12,7 +12,10 @@ $(".navbar-nav a").click(function(){
 // 	'background': 'green'
 // })
 
+total = 0
+
 $('input').on('keyup', function(){
+	total = 0
 	$('.table tr').each(function(){
 		satu = $(this).find('td:nth-child(1) .form-control').val()
 		dua = $(this).find('td:nth-child(2) .form-control').val()
@@ -22,4 +25,16 @@ $('input').on('keyup', function(){
 		// 	'background': 'green'
 		// })
 	})
+	$('.table tr td:nth-child(3) .form-control').each(function(){
+		if (parseInt($(this).val()) >= 0){
+			total += parseInt($(this).val())
+		}
+		// console.log(total)
+	})
+	$('.hasil').val(total)
+
+	var cleave = new Cleave('.hasil', {
+	    numeral: true,
+	    numeralThousandsGroupStyle: 'thousand'
+	});
 })
